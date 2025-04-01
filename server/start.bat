8ec55209b2bda2058464d0d2ec50c564e8840055
Tubix <vdmitriev556@gmail.com>
2025-04-01 19:43:16 +0100

    file server data folder name changed

--- server/start.bat
+++ server/start.bat
@@ -1,3 +1,3 @@
 cd .\TanksDemo\TanksDemo\
-dotnet run -fileServPort 8000 -resourcesPath .\..\..\..\resources\ -clientDir .\..\..\..\client\ -prod false
+dotnet run -fileServPort 8000 -resourcesPath .\..\..\..\file_server_data\ -clientDir .\..\..\..\client\ -prod false
 pause
